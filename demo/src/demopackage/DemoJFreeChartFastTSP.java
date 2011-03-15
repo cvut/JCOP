af5f14aa89f57200040de6d2e907de34f182de9f
kovaro1 <kovaro1@ca20af45-4a20-473b-94bf-0ef87cbc3059>
2011-03-15 16:24:52 +0000

    FastTSP problem demo.
    git-svn-id: svn://svn.code.sf.net/p/jcop/code/trunk@42 ca20af45-4a20-473b-94bf-0ef87cbc3059

---
+++ demo/src/demopackage/DemoJFreeChartFastTSP.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright © 2010 by Oleg Kovarik, Ondrej Skalicka. All Rights Reserved
+ */
+
+package demopackage;
+
+import cz.cvut.felk.cig.jcop.algorithm.noisingmethod.NoisingMethod;
+import cz.cvut.felk.cig.jcop.algorithm.simulatedannealing.SimulatedAnnealing;
+import cz.cvut.felk.cig.jcop.problem.tspfast.FastTSP;
+import cz.cvut.felk.cig.jcop.result.render.ExceptionRender;
+import cz.cvut.felk.cig.jcop.result.render.JFreeChartRender;
+import cz.cvut.felk.cig.jcop.result.render.SimpleCompareRender;
+import cz.cvut.felk.cig.jcop.solver.AlgorithmCompareSolver;
+import cz.cvut.felk.cig.jcop.solver.condition.TimeoutCondition;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Oleg Kovarik
+ */
+public class DemoJFreeChartFastTSP {
+    public static void main(String[] args) throws IOException {
+        AlgorithmCompareSolver solver = new AlgorithmCompareSolver(new FastTSP(new File("/home/kovarole/idea-projects/jcop/trunk/demo/data/tsp/dsj1000.tsp")));
+
+        solver.addAlgorithm(new SimulatedAnnealing(100, 0.9));
+        //solver.addAlgorithm(new GeneticAlgorithm(16, 0.2));
+        solver.addAlgorithm(new NoisingMethod(1.0, 0.9));
+
+
+        solver.addStopCondition(new TimeoutCondition(10000));
+        solver.addListener(new JFreeChartRender("Demo JFreeChartRender"));
+        solver.addRender(new SimpleCompareRender());
+        solver.addRender(new ExceptionRender());
+
+        solver.run();
+
+        solver.render();
+    }
+}
---
+++ demo/src/demopackage/DemoJFreeChartTSP.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright © 2010 by Oleg Kovarik, Ondrej Skalicka. All Rights Reserved
+ */
+
+package demopackage;
+
+import cz.cvut.felk.cig.jcop.algorithm.noisingmethod.NoisingMethod;
+import cz.cvut.felk.cig.jcop.algorithm.simulatedannealing.SimulatedAnnealing;
+import cz.cvut.felk.cig.jcop.problem.tspfast.FastTSP;
+import cz.cvut.felk.cig.jcop.result.render.ExceptionRender;
+import cz.cvut.felk.cig.jcop.result.render.JFreeChartRender;
+import cz.cvut.felk.cig.jcop.result.render.SimpleCompareRender;
+import cz.cvut.felk.cig.jcop.solver.AlgorithmCompareSolver;
+import cz.cvut.felk.cig.jcop.solver.condition.TimeoutCondition;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Oleg Kovarik
+ */
+public class DemoJFreeChartTSP {
+    public static void main(String[] args) throws IOException {
+        // AlgorithmCompareSolver solver = new AlgorithmCompareSolver(new TSPInverting(new File("/home/kovarole/idea-projects/jcop/trunk/demo/data/tsp/berlin52.tsp")));
+        AlgorithmCompareSolver solver = new AlgorithmCompareSolver(new FastTSP(new File("/home/kovarole/idea-projects/jcop/trunk/demo/data/tsp/dsj1000.tsp")));
+
+        /*
+        solver.addAlgorithm(new SimulatedAnnealing(10, 0.09));
+        solver.addAlgorithm(new SimulatedAnnealing(10, 0.9));
+        solver.addAlgorithm(new SimulatedAnnealing(10, 0.99));
+        solver.addAlgorithm(new SimulatedAnnealing(10, 0.999));
+        solver.addAlgorithm(new SimulatedAnnealing(10, 0.9999));
+        solver.addAlgorithm(new SimulatedAnnealing(100, 0.09));
+        solver.addAlgorithm(new SimulatedAnnealing(100, 0.9));
+        solver.addAlgorithm(new SimulatedAnnealing(100, 0.99));
+        solver.addAlgorithm(new SimulatedAnnealing(100, 0.999));
+        solver.addAlgorithm(new SimulatedAnnealing(100, 0.9999));
+        */
+        solver.addAlgorithm(new SimulatedAnnealing(100, 0.9));
+        solver.addAlgorithm(new SimulatedAnnealing(100, 0.999));
+        //solver.addAlgorithm(new GeneticAlgorithm(16, 0.2));
+        //solver.addAlgorithm(new RandomSearch());
+        //solver.addAlgorithm(new NoisingMethod(1.0, 0.9));
+        solver.addAlgorithm(new NoisingMethod(1.0, 0.99999));
+
+
+        solver.addStopCondition(new TimeoutCondition(30000)); // every problem 20000 iterations
+        solver.addListener(new JFreeChartRender("Demo JFreeChartRender"));
+        solver.addRender(new SimpleCompareRender());
+        solver.addRender(new ExceptionRender());
+
+        solver.run();
+
+        solver.render();
+    }
+}
